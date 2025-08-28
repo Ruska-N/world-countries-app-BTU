a570e9b8f8813deb22603f3e0bd457c2d1542698
Mariam <mariebukhaidze@gmail.com>
2025-08-28 11:14:32 +0400

    Draft functional of filter

--- script.js
+++ script.js
@@ -8,3 +8,58 @@ function toggleFilter() {
   arrowIconUp.classList.toggle("d-none");
   arrowIconDown.classList.toggle("d-none");
 }
+
+// FILTER OPTIONS IN WORK
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const filterOptions = document.querySelectorAll(
+//     ".filter-dropdown__options li"
+//   );
+//   const countriesContainer = document.querySelector(".countries");
+
+//   let countriesData = [];
+
+//   fetch("./data.json")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       countriesData = data;
+//     })
+//     .catch((err) => {
+//       console.error("Error of loading data.json:", err);
+//     });
+
+//   filterOptions.forEach((option) => {
+//     option.addEventListener("click", () => {
+//       const selectedRegion = option.textContent.trim();
+
+//       const filtered = countriesData.filter(
+//         (country) =>
+//           country.region.toLowerCase() === selectedRegion.toLowerCase()
+//       );
+
+//       renderCountries(filtered);
+//     });
+//   });
+
+//   function renderCountries(countries) {
+//     countriesContainer.innerHTML = "";
+
+//     if (countries.length === 0) {
+//       countriesContainer.innerHTML = "<p>Empty page.</p>";
+//       return;
+//     }
+
+//     countries.forEach((country) => {
+//       const countryCard = document.createElement("div");
+//       countryCard.classList.add("country-card");
+
+//       countryCard.innerHTML = `
+//         <div class="flag">${country.flag || "🏳️"}</div>
+//         <div class="name">${country.name}</div>
+//         <div class="region">${country.region}</div>
+//       `;
+
+//       countriesContainer.appendChild(countryCard);
+//     });
+//   }
+// });
