929cd3f6f1add1f4b20e87c2d83ae1c37724cb25
Ruska-N <ninidze.rusudan@gmail.com>
2025-08-29 12:33:25 +0400

    search input created

---
+++ script.js
@@ -0,0 +1,19 @@
+const searchInput = document.getElementById("searchInput");
+
+let allCountries = [];
+
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    allCountries = data;
+    displayCountries(allCountries);
+  })
+  .catch(err => console.error(err));
+
+searchInput.addEventListener("input", e => {
+  const query = e.target.value.toLowerCase();
+  const filtered = allCountries.filter(country =>
+    country.name.toLowerCase().includes(query)
+  );
+  displayCountries(filtered);
+});
